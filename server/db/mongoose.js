f076b14b3b9c6d2ac7da9ff4aeefacfe41a7e811
Niall <niall.pereira@ibm.com>
2018-06-03 21:56:34 -0400

    fix files for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 //set mongoose to use promises
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
 
+mongoose.connect('mongodb://nlp412:nlp412@ds247690.mlab.com:47690/node-todo-api' || 'mongodb://localhost:27017/TodoApp')
 module.exports = {mongoose};
